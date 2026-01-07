79ccf69f7a034bad08e0dd780c3433cf8db5f5cc
Alejandro <alejandropracticagit@hotmail.com>
2026-01-07 20:42:30 -0300

    Primer commit

---
+++ js/primer.js
@@ -0,0 +1,4 @@
+let prompt = require("prompt-sync")();
+let nombre = prompt("ingrese nombre: ");
+console.log("Welcome to the Primer.js interactive prompt!");
+console.log(`Hello, ${nombre}! Let's get started with some JavaScript coding.`);
