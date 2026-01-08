a57b63e587dd358ef1cbc349f81e54b64dac0385
Alejandro <alejandropracticagit@hotmail.com>
2026-01-07 23:31:27 -0300

    Primer archivo primer.js y index.py

--- Python/index.py
+++ Python/index.py
@@ -1,1 +1,8 @@
-print("Hola, todo bien?")
+import os
+os.system("cls" if os.name == "nt" else "clear")
+print("Cuestionario".upper())
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Encantado de conocerte, {nombre}!")
+estado = input("¿Cómo estás hoy? ")
+mensaje = "¡Me alegro de escuchar eso!" if estado.lower() == "bien" else "Espero que tu día mejore."
+print(mensaje)
--- js/primer.js
+++ js/primer.js
@@ -2,3 +2,5 @@ let prompt = require("prompt-sync")();
 let nombre = prompt("ingrese nombre: ");
 console.log("Welcome to the Primer.js interactive prompt!");
 console.log(`Hello, ${nombre}! Let's get started with some JavaScript coding.`);
+let respuesta = prompt("¿Cómo estás hoy? ");
+respuesta == "bien" ? console.log("¡Me alegro de escuchar eso!") : console.log("Espero que tu día mejore.");
